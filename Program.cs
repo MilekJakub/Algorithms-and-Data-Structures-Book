7f0c88dd06d69e2ad35cde44f67659f2ac0c6aba
Jakub Miłek <Jakub.Milek@microsoft.wsei.edu.pl>
2022-04-14 14:05:09 +0200

    Insertion sort

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 {
     static void Main()
     {
+        #region Selection sort
         //Selection sort
+        Console.WriteLine("Selection sort.");
         var tab = new int[] { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
         SortingAlgorithms.SelectionSort(tab);
         Console.WriteLine(string.Join(", ", tab));
@@ -10,7 +12,18 @@
         var stringTab = new string[] { "z", "y", "x", "a", "c", "b", "e", "d", "f" };
         SortingAlgorithms.SelectionSort<string>(stringTab);
         Console.WriteLine(string.Join(", ", stringTab));
+        #endregion
 
+        #region Insertion sort
+        //Insertion sort
+        Console.WriteLine("\nInsertion sort.");
+        tab = new int[] { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+        SortingAlgorithms.InsertionSort(tab);
+        Console.WriteLine(string.Join(", ", tab));
 
+        stringTab = new string[] { "z", "y", "x", "a", "c", "b", "e", "d", "f" };
+        SortingAlgorithms.InsertionSort<string>(stringTab);
+        Console.WriteLine(string.Join(", ", stringTab));
+        #endregion
     }
 }
--- SortingAlgorithms.cs
+++ SortingAlgorithms.cs
@@ -50,4 +50,32 @@
             }
         }
     }
+    public static void InsertionSort(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j])
+            {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+    public static void InsertionSort<T>(T[] arr) where T : IComparable
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            int j = i;
+            while (j > 0 && arr[j - 1].CompareTo(arr[j]) > 0)
+            {
+                T temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
 }
