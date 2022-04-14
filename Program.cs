1dcb734becdada34faa8c6ba6a7211e86c64f628
Jakub Miłek <Jakub.Milek@microsoft.wsei.edu.pl>
2022-04-14 12:53:13 +0200

    Project files with Selection Sort

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        //Selection sort
+        var tab = new int[] { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+        SortingAlgorithms.SelectionSort(tab);
+        Console.WriteLine(string.Join(", ", tab));
+
+        var stringTab = new string[] { "z", "y", "x", "a", "c", "b", "e", "d", "f" };
+        SortingAlgorithms.SelectionSort<string>(stringTab);
+        Console.WriteLine(string.Join(", ", stringTab));
+
+
+    }
+}
---
+++ SortingAlgorithms.cs
@@ -0,0 +1,53 @@
+﻿public static class SortingAlgorithms
+{
+    public static void SelectionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int min = arr[i];
+            int index = i;
+
+            for (int j = i; j < arr.Length; j++)
+            {
+                if (arr[j] <= min)
+                {
+                    min = arr[j];
+                    index = j;
+                }
+            }
+
+            if (index != i)
+            {
+                int temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+                min = arr[i];
+            }
+        }
+    }
+    public static void SelectionSort<T>(T[] arr) where T : IComparable
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            T min = arr[i];
+            int index = i;
+
+            for (int j = i; j < arr.Length; j++)
+            {
+                if (min.CompareTo(arr[j]) > 0)
+                {
+                    min = arr[j];
+                    index = j;
+                }
+            }
+
+            if (index != i)
+            {
+                T temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+                min = arr[i];
+            }
+        }
+    }
+}
